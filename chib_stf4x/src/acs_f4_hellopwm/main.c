6a0a792afbe6fcadd8a131aeafe9d34323d72503
Chad Coates <chroco@gmail.com>
2018-02-06 22:29:04 -0800

    testing pwm

--- chib_stf4x/src/acs_f4_hellopwm/main.c
+++ chib_stf4x/src/acs_f4_hellopwm/main.c
@@ -63,7 +63,7 @@ static PWMConfig pwmcfg = {
 
 // Input Capture Unit (ICU)
 // http://chibios.sourceforge.net/html/group___i_c_u.html
-
+/*
 icucnt_t last_width, last_period; // ICU counter type
 
 static void icuwidthcb(ICUDriver *icup) { // ICU width callback
@@ -80,14 +80,14 @@ static void icuperiodcb(ICUDriver *icup) { // ICU period callback
 
 static ICUConfig icucfg = {
   ICU_INPUT_ACTIVE_HIGH,
-  10000,                                    /* 10kHz ICU clock frequency.   */
+  10000,                                    // 10kHz ICU clock frequency. 
   icuwidthcb,
   icuperiodcb,
   NULL,
   ICU_CHANNEL_1,
   0
 };
-
+*/
 /*
  * Application entry point.
  */
@@ -114,10 +114,10 @@ int main(void) {
   pwmStart(&PWMD1, &pwmcfg);
   pwmEnablePeriodicNotification(&PWMD1);
   palSetPadMode(GPIOA, 8, PAL_MODE_ALTERNATE(1));
-  icuStart(&ICUD3, &icucfg);
-  palSetPadMode(GPIOC, 6, PAL_MODE_ALTERNATE(2));
-  icuStartCapture(&ICUD3);
-  icuEnableNotifications(&ICUD3);
+//  icuStart(&ICUD3, &icucfg);
+//  palSetPadMode(GPIOC, 6, PAL_MODE_ALTERNATE(2));
+//  icuStartCapture(&ICUD3);
+//  icuEnableNotifications(&ICUD3);
   chThdSleepMilliseconds(2000);
 
   /*
@@ -130,31 +130,34 @@ int main(void) {
   /*
    * Changes the PWM channel 0 to 50% duty cycle.
    */
-  pwmEnableChannel(&PWMD1, 0, PWM_PERCENTAGE_TO_WIDTH(&PWMD1, 5000));
-  chThdSleepMilliseconds(5000);
+//  pwmEnableChannel(&PWMD1, 0, PWM_PERCENTAGE_TO_WIDTH(&PWMD1, 5000));
+ // chThdSleepMilliseconds(5000);
 
   /*
    * Changes the PWM channel 0 to 25% duty cycle.
    */
-  pwmEnableChannel(&PWMD1, 0, PWM_PERCENTAGE_TO_WIDTH(&PWMD1, 2500));
-  chThdSleepMilliseconds(5000);
+//  pwmEnableChannel(&PWMD1, 0, PWM_PERCENTAGE_TO_WIDTH(&PWMD1, 2500));
+//  chThdSleepMilliseconds(5000);
 
   /*
    * Changes PWM period to half second the duty cycle becomes 50%
    * implicitly.
    */
-  pwmChangePeriod(&PWMD1, 5000);
-  chThdSleepMilliseconds(5000);
+//  pwmChangePeriod(&PWMD1, 5000);
+//  chThdSleepMilliseconds(5000);
 
   /*
    * Disables channel 0 and stops the drivers.
    */
-  pwmDisableChannel(&PWMD1, 0);
-  pwmStop(&PWMD1);
-  icuStopCapture(&ICUD3);
-  icuStop(&ICUD3);
-  palClearPad(GPIOD, GPIOD_PIN4);
-  palClearPad(GPIOD, GPIOD_PIN5);
+	/*
+	 * Lets keep it going instead!
+	 */
+//  pwmDisableChannel(&PWMD1, 0);
+//  pwmStop(&PWMD1);
+//  icuStopCapture(&ICUD3);
+//  icuStop(&ICUD3);
+//////////////  palClearPad(GPIOD, GPIOD_PIN4);
+//  palClearPad(GPIOD, GPIOD_PIN5);
 
   /*
    * Normal main() thread activity, in this demo it does nothing.
