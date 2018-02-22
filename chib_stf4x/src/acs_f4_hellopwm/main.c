6d108399468d691f8f5588c691831f20909bed20
Max Schweitzer <mschwei2@pdx.edu>
2018-02-21 23:44:49 -0800

    First test of three PWM channels Outputs on PA8 PA9 and PA10

--- chib_stf4x/src/acs_f4_hellopwm/main.c
+++ chib_stf4x/src/acs_f4_hellopwm/main.c
@@ -17,24 +17,55 @@
 #include "ch.h"
 #include "hal.h"
 
+static int cycle0 = 1000;
+static int cycle1 = 1000;
+static int cycle2 = 1000;
+
 static void pwmpcb(PWMDriver *pwmp) { // period call back
   (void)pwmp;
-  palClearPad(GPIOA, GPIOA_LED_GREEN);
+  //palClearPad(GPIOA, GPIOA_LED_GREEN);
+  //palClearPad(GPIOA, GPIOA_PIN15);
+  //palClearPad(GPIOA, 9);
+  //pwmEnableChannel(&PWMD1, 0, PWM_PERCENTAGE_TO_WIDTH(&PWMD1, 1000));
+  //pwmEnableChannel(&PWMD1, 1, PWM_PERCENTAGE_TO_WIDTH(&PWMD1, 1000));
+  //pwmEnableChannel(&PWMD1, 2, PWM_PERCENTAGE_TO_WIDTH(&PWMD1, 1000));
 }
 
 static void pwmc1cb(PWMDriver *pwmp) { // channel 1 callback
   (void)pwmp;
-  palSetPad(GPIOA, GPIOA_LED_GREEN);
+  //palSetPad(GPIOA, 10);
+  pwmEnableChannelI(&PWMD1, 0, PWM_PERCENTAGE_TO_WIDTH(&PWMD1, cycle0));
+  cycle0 += 10;
+  if (cycle0 > 10000)
+    cycle0 = 0;
+}
+
+static void pwmc2cb(PWMDriver *pwmp) { // channel 1 callback
+  (void)pwmp;
+  //palSetPad(GPIOA, 8);
+  pwmEnableChannelI(&PWMD1, 1, PWM_PERCENTAGE_TO_WIDTH(&PWMD1, cycle1));
+  cycle1 += 100;
+  if (cycle1 > 10000)
+    cycle1 = 0;
+}
+
+static void pwmc3cb(PWMDriver *pwmp) { // channel 1 callback
+  (void)pwmp;
+  //palSetPad(GPIOA, 9);
+  pwmEnableChannelI(&PWMD1, 2, PWM_PERCENTAGE_TO_WIDTH(&PWMD1, cycle2));
+  cycle2 += 1000;
+  if (cycle2 > 10000)
+    cycle2 = 0;
 }
 
 static PWMConfig pwmcfg = {
-  10000,                                    /* 10kHz PWM clock frequency.   */
-  10000,                                    /* Initial PWM period 1S.       */
+  40000,                                    /* 10kHz PWM clock frequency.   */
+  1000,                                    /* Initial PWM period 1S.       */
   pwmpcb,
   {
    {PWM_OUTPUT_ACTIVE_HIGH, pwmc1cb},
-   {PWM_OUTPUT_DISABLED, NULL},
-   {PWM_OUTPUT_DISABLED, NULL},
+   {PWM_OUTPUT_ACTIVE_HIGH, pwmc2cb},
+   {PWM_OUTPUT_ACTIVE_HIGH, pwmc3cb},
    {PWM_OUTPUT_DISABLED, NULL}
   },
   0,
@@ -48,14 +79,20 @@ static THD_FUNCTION(pwmThread1, arg) {
   chRegSetThreadName("pwm");
 	// initialize pwm driver
   pwmStart(&PWMD1, &pwmcfg);
-  pwmEnablePeriodicNotification(&PWMD1);
+  //pwmEnablePeriodicNotification(&PWMD1);
   palSetPadMode(GPIOA, 8, PAL_MODE_ALTERNATE(1));
+  palSetPadMode(GPIOA, 9, PAL_MODE_ALTERNATE(1));
+  palSetPadMode(GPIOA, 10, PAL_MODE_ALTERNATE(1));
   chThdSleepMilliseconds(2000);
   //Starts the PWM channel 0 using 50% duty cycle.
-  pwmEnableChannel(&PWMD1, 0, PWM_PERCENTAGE_TO_WIDTH(&PWMD1, 5000));
+  pwmEnableChannel(&PWMD1, 0, PWM_PERCENTAGE_TO_WIDTH(&PWMD1, 1000));
+  pwmEnableChannel(&PWMD1, 1, PWM_PERCENTAGE_TO_WIDTH(&PWMD1, 5000));
+  pwmEnableChannel(&PWMD1, 2, PWM_PERCENTAGE_TO_WIDTH(&PWMD1, 9000));
 //	pwmChangePeriod (&PWMD1, 5000);
   pwmEnableChannelNotification(&PWMD1, 0);
-  chThdSleepMilliseconds(5000);
+  pwmEnableChannelNotification(&PWMD1, 1);
+  pwmEnableChannelNotification(&PWMD1, 2);
+  chThdSleepMilliseconds(500);
 	/*
    * Disables channel 0 and stops the drivers.
    */
@@ -73,7 +110,9 @@ static THD_FUNCTION(pwmThread1, arg) {
 int main(void) {
   halInit(); 
   chSysInit(); 
-  palSetPadMode(GPIOA, GPIOA_LED_GREEN, PAL_MODE_OUTPUT_PUSHPULL);
+  palSetPadMode(GPIOA, 10, PAL_MODE_OUTPUT_PUSHPULL);
+  palSetPadMode(GPIOA, 8, PAL_MODE_OUTPUT_PUSHPULL);
+  palSetPadMode(GPIOA, 9, PAL_MODE_OUTPUT_PUSHPULL);
 
   /*
    * Initializes the PWM driver
